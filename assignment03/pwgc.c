ceb35a4f8142caf0f1007c353cbd3fc246e7dffa
LDevine56 <sd081104@gmail.com>
2022-05-21 01:17:16 +0900

    build: (1)

--- assignment03/pwgc.c
+++ assignment03/pwgc.c
@@ -2,95 +2,176 @@
 #include <stdlib.h>
 
 #define PEASANT 0x08
-#define WOLF	0x04
-#define GOAT	0x02
-#define CABBAGE	0x01
+#define WOLF 0x04
+#define GOAT 0x02
+#define CABBAGE 0x01
 
 // 주어진 상태 state의 이름(마지막 4비트)을 화면에 출력
 // 예) state가 7(0111)일 때, "<0111>"을 출력
-static void print_statename( FILE *fp, int state);
+static void print_statename(FILE *fp, int state);
 
 // 주어진 상태 state에서 농부, 늑대, 염소, 양배추의 상태를 각각 추출하여 p, w, g, c에 저장
 // 예) state가 7(0111)일 때, p = 0, w = 1, g = 1, c = 1
-static void get_pwgc( int state, int *p, int *w, int *g, int *c);
+static void get_pwgc(int state, int *p, int *w, int *g, int *c);
 
 // 허용되지 않는 상태인지 검사
 // 예) 농부없이 늑대와 염소가 같이 있는 경우 / 농부없이 염소와 양배추가 같이 있는 경우
 // return value: 1 허용되지 않는 상태인 경우, 0 허용되는 상태인 경우
-static int is_dead_end( int state);
+static int is_dead_end(int state);
 
 // state1 상태에서 state2 상태로의 전이 가능성 점검
 // 농부 또는 농부와 다른 하나의 아이템이 강 반대편으로 이동할 수 있는 상태만 허용
 // 허용되지 않는 상태(dead-end)로의 전이인지 검사
-// return value: 1 전이 가능한 경우, 0 전이 불이가능한 경우 
-static int is_possible_transition( int state1,	int state2);
+// return value: 1 전이 가능한 경우, 0 전이 불이가능한 경우
+static int is_possible_transition(int state1, int state2);
 
 // 상태 변경: 농부 이동
 // return value : 새로운 상태
-static int changeP( int state);
+static int changeP(int state);
 
 // 상태 변경: 농부, 늑대 이동
 // return value : 새로운 상태, 상태 변경이 불가능한 경우: -1
-static int changePW( int state);
+static int changePW(int state);
 
 // 상태 변경: 농부, 염소 이동
 // return value : 새로운 상태, 상태 변경이 불가능한 경우: -1
-static int changePG( int state);
+static int changePG(int state);
 
 // 상태 변경: 농부, 양배추 이동
-// return value : 새로운 상태, 상태 변경이 불가능한 경우: -1 
-static int changePC( int state);
+// return value : 새로운 상태, 상태 변경이 불가능한 경우: -1
+static int changePC(int state);
 
 // 주어진 state가 이미 방문한 상태인지 검사
 // return value : 1 visited, 0 not visited
-static int is_visited( int visited[], int depth, int state);
+static int is_visited(int visited[], int depth, int state);
 
 // 방문한 경로(상태들)을 차례로 화면에 출력
-static void print_path( int visited[], int depth);
+static void print_path(int visited[], int depth);
 
 // recursive function
-static void dfs_main( int initial_state, int goal_state, int depth, int visited[]);
+static void dfs_main(int initial_state, int goal_state, int depth, int visited[]);
 
 ////////////////////////////////////////////////////////////////////////////////
 // 상태들의 인접 행렬을 구하여 graph에 저장
 // 상태간 전이 가능성 점검
-// 허용되지 않는 상태인지 점검 
-void make_adjacency_matrix( int graph[][16]);
+// 허용되지 않는 상태인지 점검
+void make_adjacency_matrix(int graph[][16]);
 
 // 인접행렬로 표현된 graph를 화면에 출력
-void print_graph( int graph[][16], int num);
+void print_graph(int graph[][16], int num);
 
 // 주어진 그래프(graph)를 .net 파일로 저장
 // pgwc.net 참조
-void save_graph( char *filename, int graph[][16], int num);
+void save_graph(char *filename, int graph[][16], int num);
 
 ////////////////////////////////////////////////////////////////////////////////
 // 깊이 우선 탐색 (초기 상태 -> 목적 상태)
-void depth_first_search( int initial_state, int goal_state)
+void depth_first_search(int initial_state, int goal_state)
 {
 	int depth = 0;
-	int visited[16] = {0,}; // 방문한 정점을 저장
-	
-	//dfs_main( initial_state, goal_state, depth, visited); 
+	int visited[16] = {
+		0,
+	}; // 방문한 정점을 저장
+
+	// dfs_main( initial_state, goal_state, depth, visited);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int main( int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int graph[16][16] = {0,};
-	
+	int graph[16][16] = {
+		0,
+	};
+
 	// 인접 행렬 만들기
-	//make_adjacency_matrix( graph);
+	// make_adjacency_matrix( graph);
 
 	// 인접 행렬 출력 (only for debugging)
-	//print_graph( graph, 16);
-	
+	// print_graph( graph, 16);
+
 	// .net 파일 만들기
-	//save_graph( "pwgc.net", graph, 16);
+	// save_graph( "pwgc.net", graph, 16);
 
 	// 깊이 우선 탐색
-	//depth_first_search( 0, 15); // initial state, goal state
-	
+	// depth_first_search( 0, 15); // initial state, goal state
+
 	return 0;
 }
 
+// 주어진 상태 state의 이름(마지막 4비트)을 화면에 출력
+// 예) state가 7(0111)일 때, "<0111>"을 출력
+// file=printf, 출력을 파일에 작성
+// int -> char: + '0'
+static void print_statename(FILE *fp, int state)
+{
+	int p, w, g, c;
+	get_pwgc(state, &p, &w, &g, &c);
+	char state_bit[4];
+	state_bit[0] = p + '0';
+	state_bit[1] = w + '0';
+	state_bit[2] = g + '0';
+	state_bit[3] = c + '0';
+	fprintf(fp, "<%s>", state_bit);
+}
+
+// 주어진 상태 state에서 농부, 늑대, 염소, 양배추의 상태를 각각 추출하여 p, w, g, c에 저장
+// 예) state가 7(0111)일 때, p = 0, w = 1, g = 1, c = 1
+// 1000: 8, 0100: 4, 0010: 2, 0001: 1
+// 연산자 우선순위 >>, &
+static void get_pwgc(int state, int *p, int *w, int *g, int *c)
+{
+	*p = (state & PEASANT) >> 3;
+	*w = (state & WOLF) >> 2;
+	*g = (state & GOAT) >> 1;
+	*c = state & CABBAGE;
+}
+
+// 허용되지 않는 상태인지 검사
+// 예) 농부없이 늑대와 염소가 같이 있는 경우 / 농부없이 염소와 양배추가 같이 있는 경우
+// return value: 1 허용되지 않는 상태인 경우, 0 허용되는 상태인 경우
+static int is_dead_end(int state);
+
+// state1 상태에서 state2 상태로의 전이 가능성 점검
+// 농부 또는 농부와 다른 하나의 아이템이 강 반대편으로 이동할 수 있는 상태만 허용
+// 허용되지 않는 상태(dead-end)로의 전이인지 검사
+// return value: 1 전이 가능한 경우, 0 전이 불이가능한 경우
+static int is_possible_transition(int state1, int state2);
+
+// 상태 변경: 농부 이동
+// return value : 새로운 상태
+static int changeP(int state);
+
+// 상태 변경: 농부, 늑대 이동
+// return value : 새로운 상태, 상태 변경이 불가능한 경우: -1
+static int changePW(int state);
+
+// 상태 변경: 농부, 염소 이동
+// return value : 새로운 상태, 상태 변경이 불가능한 경우: -1
+static int changePG(int state);
+
+// 상태 변경: 농부, 양배추 이동
+// return value : 새로운 상태, 상태 변경이 불가능한 경우: -1
+static int changePC(int state);
+
+// 주어진 state가 이미 방문한 상태인지 검사
+// return value : 1 visited, 0 not visited
+static int is_visited(int visited[], int depth, int state);
+
+// 방문한 경로(상태들)을 차례로 화면에 출력
+static void print_path(int visited[], int depth);
+
+// recursive function
+static void dfs_main(int initial_state, int goal_state, int depth, int visited[]);
+
+////////////////////////////////////////////////////////////////////////////////
+// 상태들의 인접 행렬을 구하여 graph에 저장
+// 상태간 전이 가능성 점검
+// 허용되지 않는 상태인지 점검
+void make_adjacency_matrix(int graph[][16]);
+
+// 인접행렬로 표현된 graph를 화면에 출력
+void print_graph(int graph[][16], int num);
+
+// 주어진 그래프(graph)를 .net 파일로 저장
+// pgwc.net 참조
+void save_graph(char *filename, int graph[][16], int num);

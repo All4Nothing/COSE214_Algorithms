09f3f737cc6ee207c1a50a64f02cbb865a2f5232
LDevine56 <sd081104@gmail.com>
2022-06-05 21:08:51 +0900

    build: (1)

--- assignment04/editdistance.c
+++ assignment04/editdistance.c
@@ -105,3 +105,41 @@ int main()
 	return 0;
 }
 
+// 재귀적으로 연산자 행렬을 순회하며, 두 문자열이 최소편집거리를 갖는 모든 가능한 정렬(alignment) 결과를 출력한다.
+// op_matrix : 이전 상태의 연산자 정보가 저장된 행렬 (1차원 배열임에 주의!)
+// col_size : op_matrix의 열의 크기
+// str1 : 문자열 1
+// str2 : 문자열 2
+// n : 문자열 1의 길이
+// m : 문자열 2의 길이
+// level : 재귀호출의 레벨 (0, 1, 2, ...)
+// align_str : 정렬된 문자쌍들의 정보가 저장된 문자열 배열 예) "a - a", "a - b", "* - b", "ab - ba"
+static void backtrace_main( int *op_matrix, int col_size, char *str1, char *str2, int n, int m, int level, char align_str[][8]);
+
+// 강의 자료의 형식대로 op_matrix를 출력 (좌하단(1,1) -> 우상단(n, m))
+// 각 연산자를 다음과 같은 기호로 표시한다. 삽입:I, 삭제:D, 교체:S, 일치:M, 전위:T
+void print_matrix( int *op_matrix, int col_size, char *str1, char *str2, int n, int m){
+	int operation;
+
+	for(int i = n; i>0;i--){
+		for(int j=1; j<m;j++){
+			operation = op_matrix[i * col_size + j];
+			if(operation & INSERT_OP)
+			{
+				
+			}
+		}
+	}
+}
+
+// 두 문자열 str1과 str2의 최소편집거리를 계산한다.
+// return value : 최소편집거리
+// 이 함수 내부에서 print_matrix 함수와 backtrace 함수를 호출함
+int min_editdistance( char *str1, char *str2){
+	int n = strlen(str1);
+	int m = strlen(str2);
+
+	int i, j;
+	int d[n + 1][m + 1];
+	int op_matrix[(n+1) * (m+1)];
+}
